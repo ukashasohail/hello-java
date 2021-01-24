e04e24b9101d6f90df2d645ecb6582f0272d35ef
ukashasohail <ukasha909@outlook.com>
2021-01-24 19:58:19 +0500

    :zap:

---
+++ lab13/MyServer.java
@@ -0,0 +1,13 @@
+import java.rmi.*;
+import java.rmi.registry.*;
+
+public class MyServer {
+    public static void main(String args[]) {
+        try {
+            Adder stub = new AdderRemote();
+            Naming.rebind("rmi://localhost:5000/lab", stub);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
